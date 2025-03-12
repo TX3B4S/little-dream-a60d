8b7476e1f27417b8be5a0f5695c69cc6b26862d8
TX3B4S <56360175+TX3B4S@users.noreply.github.com>
2025-03-12 09:54:51 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "A depiction of Emmanuel, the sacred name of the Messiah, symbolizing 'God with us', with a serene and peaceful atmosphere, highlighting the significance of God's presence among humanity. -Without text -epic style -realistic acuarela painting style",
     };
 
     const response = await env.AI.run(
